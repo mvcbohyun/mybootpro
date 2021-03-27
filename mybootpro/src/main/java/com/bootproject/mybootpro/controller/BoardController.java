42368066ff97046ebd8446d4c65797f8cc0ae9b1
mvcbohyun <jbh959847@gmail.com>
2021-03-27 15:30:16 +0900

    게시판 validator 처리 및 게시판 작성 추가

--- mybootpro/src/main/java/com/bootproject/mybootpro/controller/BoardController.java
+++ mybootpro/src/main/java/com/bootproject/mybootpro/controller/BoardController.java
@@ -1,14 +1,21 @@
 package com.bootproject.mybootpro.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bootproject.mybootpro.model.Board;
 import com.bootproject.mybootpro.repository.BoardRepository;
+import com.bootproject.mybootpro.validator.BoardValidator;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,11 +25,39 @@ import lombok.RequiredArgsConstructor;
 public class BoardController {
 
 	private final BoardRepository boardRepository;
+	private final BoardValidator boardbalidator;
 	
-	@GetMapping("list")
+	@GetMapping("/list")
 	public String list(Model model) {
 	List<Board> boards= boardRepository.findAll();
 	model.addAttribute("boards",boards);
 	return "board/list";
 	}
+	
+	@GetMapping("/form")
+	public String form(@RequestParam(required = false)Long id,Model model) {
+		if(id == null) {
+			model.addAttribute("board",new Board());
+			
+		}else {
+			Board board =boardRepository.findById(id).orElse(null);
+			model.addAttribute("board",board);
+		}
+
+	return "board/form";
+	}
+	
+	@PostMapping("/form")
+	  public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+		boardbalidator.validate(board, bindingResult);
+		System.out.println(bindingResult.hasErrors());
+		if(bindingResult.hasErrors()) {
+			return "board/form";
+		}
+		
+		board.setJoindate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+		board.setEnabled("Y");
+	    boardRepository.save(board);
+	    return "redirect:/board/list";
+	  }
 }
--- mybootpro/src/main/java/com/bootproject/mybootpro/model/Board.java
+++ mybootpro/src/main/java/com/bootproject/mybootpro/model/Board.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -13,6 +15,9 @@ public class Board {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotNull
+	@Size(min=2, max=30)
 	private String title;
 	private String content;
 	private String joindate;
