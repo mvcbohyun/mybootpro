5fafbf55645219416dc9acc023bcd983f535ec3e
mvcbohyun <jbh959847@gmail.com>
2021-03-27 15:30:42 +0900

    게시판 restapi 추가

--- mybootpro/src/main/java/com/bootproject/mybootpro/repository/BoardRepository.java
+++ mybootpro/src/main/java/com/bootproject/mybootpro/repository/BoardRepository.java
@@ -1,5 +1,7 @@
 package com.bootproject.mybootpro.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bootproject.mybootpro.model.Board;
@@ -8,4 +10,7 @@ import com.bootproject.mybootpro.model.Board;
 
 public interface BoardRepository  extends JpaRepository<Board, Long>{
 
+	
+	List<Board> findByTitle(String title);
+	List<Board> findByTitleOrContent(String title , String content);
 }
